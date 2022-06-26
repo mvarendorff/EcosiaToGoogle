6fbaed7cb876c9a5f51b3aeb3ad5008449d144fe
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2022-06-26 18:24:40 +0200

    Update EcosiaToGoogle.user.js

--- EcosiaToGoogle.user.js
+++ EcosiaToGoogle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ecosia to google.
 // @namespace    https://github.com/geisterfurz007
-// @version      1.0
+// @version      1.1
 // @description  Link to google with the same search from Ecosia.
 // @author       geisterfurz007
 // @match        https://www.ecosia.org/search*
@@ -11,13 +11,17 @@
 'use strict';
 
 const injectButton = () => {
+    const elementId = "ecosia-to-google-debug";
+    const injectedButton = document.getElementById(elementId);
+    if (injectedButton) return;
+
     const urlRegex = /https.*?search\?.*?(q=.*)/
     const query = window.location.toString().match(urlRegex)[1];
 
-    const reference = document.querySelector(".main-header__install-cta");
+    const reference = document.querySelector(".main-header__install-cta") || document.querySelector(".personal-counter").parentElement;
     const navBar = reference.parentElement;
 
-    const styleSource = reference.querySelector("a");
+    const styleSource = document.querySelector(".personal-counter button");
 
     const a = document.createElement("a");
     for (const {name, value} of styleSource.attributes) {
@@ -26,9 +30,14 @@ const injectButton = () => {
 
     a.href = "https://google.com/search?" + query;
     a.innerText = "Link to Google";
-    a.id = "ecosia-to-google-debug";
+    a.id = elementId;
 
+    console.log('Inserting button');
     navBar.insertBefore(a, reference);
 };
 
-window.onload = injectButton;
+window.addEventListener('load', function() {
+    injectButton();
+});
+
+injectButton();
