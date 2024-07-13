736512fcc37c81d8cd3f9dbb6b2a65eb5e5efaf5
geisterfurz007 <geisterfurz007@users.noreply.github.com>
2024-07-13 20:52:04 +0200

    Stop relying on existing styles
    Copying the styles from another button became more and more tedious and recently impossible to do because there were no more buttons to steal from. So the button is now styled with custom styling that at least uses some of the available CSS variables.

--- EcosiaToGoogle.user.js
+++ EcosiaToGoogle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ecosia to google.
 // @namespace    https://github.com/geisterfurz007
-// @version      1.3
+// @version      1.4
 // @description  Link to google with the same search from Ecosia.
 // @author       geisterfurz007
 // @match        https://www.ecosia.org/search*
@@ -11,7 +11,7 @@
 'use strict';
 
 const injectButton = () => {
-    const elementId = "ecosia-to-google-debug";
+    const elementId = "ecosia-to-google";
     const injectedButton = document.getElementById(elementId);
     if (injectedButton) return;
 
@@ -21,20 +21,30 @@ const injectButton = () => {
     const reference = document.querySelector(".main-header__space") || document.querySelector(".main-header__install-cta") || document.querySelector(".personal-counter").parentElement;
     const navBar = reference.parentElement;
 
-    const styleSource = document.querySelector(".personal-counter button") || document.querySelector(".personal-counter-interim button");
-
-    const a = document.createElement("a");
-    for (const {name, value} of styleSource.attributes) {
-        a.setAttribute(name, value);
+    const injectedStyle = `
+    #${elementId} {
+      width: unset;
+      border: var(--color-brand-primary) solid 1px;
+      border-radius: 100px;
+      padding: .5rem 1rem;
     }
 
+    #${elementId}:hover {
+      background: var(--color-button-background-transparent-hover);
+    }
+    `;
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = injectedStyle;
+    document.head.appendChild(styleTag);
+
+    const a = document.createElement("a");
     a.href = "https://google.com/search?" + query;
     a.innerText = "Link to Google";
     a.id = elementId;
     a.style.width = "unset";
 
-    console.log('Inserting button');
-    navBar.insertBefore(a, reference);
+    console.log('Inserting button', reference);
+    setTimeout(() => navBar.insertBefore(a, reference), 300);
 };
 
 window.addEventListener('load', function() {
